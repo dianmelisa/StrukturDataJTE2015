68e472a24c36c904ed1183c04f673d8959b3c467
Dian Melisa M <dian.melisa@elektro-unsyiah.net>
2015-09-12 21:01:37 +0700

    penerapan inheritance pada java

---
+++ Prak-05-Kebun-Binatang/Tumbuhan.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of interface Tumbuhan here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Tumbuhan
+{
+    /**
+     * An example of a method header - replace this comment with your own
+     * 
+     * @param  y    a sample parameter for a method
+     * @return        the result produced by sampleMethod 
+     */
+    public boolean berbuah();
+}
