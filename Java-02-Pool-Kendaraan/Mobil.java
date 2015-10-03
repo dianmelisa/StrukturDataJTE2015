5ed593577f3d4df14cfdfdab5fe58673250b25e7
Dian Melisa M <dian.melisa@elektro-unsyiah.net>
2015-10-03 17:21:13 +0700

    Review Java-02

---
+++ Java-02-Pool-Kendaraan/Mobil.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Mobil here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mobil implements Kendaraan
+{
+    private String plat;
+     
+    
+    public Mobil(String plat){
+        this.plat=plat;
+        
+    }
+    
+    public String plat(){
+        return plat;
+    }
+    
+    public String tipe(){
+        return "Mobil";
+    }
+    
+   
+    
+}
---
+++ Java-02-Pool-Kendaraan/SepedaMotor.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class SepedaMotor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SepedaMotor implements Kendaraan
+{
+    private String plat;
+    
+    public SepedaMotor(String plat){
+        this.plat=plat;
+        
+    }
+    
+    public String plat(){
+        return plat;
+    }
+    
+    public String tipe(){
+        return "SepedaMotor";
+    }
+    
+}
